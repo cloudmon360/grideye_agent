eccb48cfdbf2642fd8c5f53cbfc48a9901d0977b
Kristofer Hallin <kristofer.hallin@cloudmon360.com>
2018-04-13 20:22:57 +0200

    Added comment

--- grideye_agent.c
+++ grideye_agent.c
@@ -422,7 +422,7 @@ url_post(char *url,
      * verification of the server's certificate. This makes the connection
      * A LOT LESS SECURE.
      */
-#if 0
+#if 0 /* Verify CA */
     curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
 #endif
     if (debug>1)
