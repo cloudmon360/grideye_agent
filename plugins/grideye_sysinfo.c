a7b305579115c0c2133ec166950d189c76a84fdc
Kristofer Hallin <kristofer.hallin@cloudmon360.com>
2018-04-26 20:25:45 +0200

    Get load in percent for the CPU core we are currently executing on

--- plugins/grideye_sysinfo.c
+++ plugins/grideye_sysinfo.c
@@ -21,6 +21,7 @@
 #define LINUX_SYSINFO_LOADS_SCALE (65536)
 #define PERCENT (100)
 #define DEC_3 (1000)
+#define BUF_MAX 1024
 
 /* Forward */
 int  sysinfo_test(char *instr, char **outstr);
@@ -39,6 +40,69 @@ static const struct grideye_plugin_api api = {
     NULL
 };
 
+/*
+ * Get the CPU we're executing on
+ */
+int
+get_cpu(pid_t pid)
+{
+    int cpu = -1;
+    char pidstat[128];
+
+    FILE *fd;
+    char buffer[BUF_MAX];
+
+    sprintf(pidstat, "/proc/%d/stat", pid);
+
+    if ((fd = fopen(pidstat, "r")) == NULL)
+	return -1;
+    if (!fgets (buffer, BUF_MAX, fd)) {
+	perror ("Error"); }
+    if (sscanf (buffer, "%*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s" \
+		"%*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s" \
+		"%*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %d", &cpu) == -1)
+	return -1;
+
+    return cpu;
+}
+
+/*
+ * Get CPU load in percent
+ */
+double
+get_cpu_load(int cpu)
+{
+    FILE *fp;
+    char buffer[1024];
+    char command[128];
+    float usr;
+
+    if (cpu == -1) {
+	sprintf(command, "mpstat 1 1");
+    } else {
+	sprintf(command, "mpstat -P %d 1 1", cpu);
+    }
+
+    if (access("/usr/bin/mpstat", 0) == -1)
+      return -1;
+
+    if ((fp = popen(command, "r")) == NULL) {
+	fprintf(stderr, "Failed to run command mpstat\n" );
+	return -1;
+    }
+
+    while (fgets(buffer, sizeof(buffer)-1, fp) != NULL) {
+	if (cpu != -1)
+	    sscanf(buffer, "Average:\t%*d\t%f", &usr);
+	else
+	    sscanf(buffer, "Average:\tall\t%f", &usr);
+    }
+
+    pclose(fp);
+
+    return usr;
+}
+
 /*! Poll sysinfo command for system status
  * @param[out]  outstr  XML string with three parameters described below
  * The string contains the following parameters (or possibly a subset):
@@ -48,13 +112,14 @@ static const struct grideye_plugin_api api = {
  * iwfreq
  * iwproto
  */
-int  
+int
 sysinfo_test(char      *instr,
 	     char     **outstr)
 {
     int             retval = -1;
     struct sysinfo  info;
     unsigned long   uptime;
+    double          load_core = 0;
     unsigned long   loads;
     unsigned long   freeram;
     unsigned long   usedram;
@@ -63,34 +128,45 @@ sysinfo_test(char      *instr,
     unsigned long   freeswap;
     unsigned long   usedswap;
     int             memunit;
-    char           *str = NULL;
+    char            *str = NULL;
     size_t          slen;
+    pid_t           pid;
 
     if (sysinfo(&info) < 0){
 	perror("sysinfo");
 	goto done;
     }
+
     memunit = info.mem_unit;
     uptime = info.uptime;
+    pid = getpid();
+
     loads = ((info.loads[0]*PERCENT*DEC_3)/LINUX_SYSINFO_LOADS_SCALE);
+    load_core = get_cpu_load(get_cpu(pid));
+
+    if (load_core == -1)
+      load_core = 0.0;
+
     freeram = info.freeram*memunit;
     usedram = info.totalram*memunit - info.freeram*memunit;
     bufferram = info.bufferram*memunit;
     procs = info.procs;
     freeswap = info.freeswap*memunit;
     usedswap = info.totalswap*memunit - info.freeswap*memunit;
-    
+
     if ((slen = snprintf(NULL, 0,
 			 "<uptime>%lu</uptime>"
 			 "<loads>%lu.%lu</loads>"
+			 "<loadcore>%f</loadcore>"
 			 "<freeram>%lu</freeram>"
 			 "<usedram>%lu</usedram>"
 			 "<bufferram>%lu</bufferram>"
 			 "<procs>%lu</procs>"
 			 "<freeswap>%lu</freeswap>"
-			 "<usedswap>%lu</usedswap>", 
+			 "<usedswap>%lu</usedswap>",
 			 uptime,
 			 loads/DEC_3, loads%DEC_3,
+			 load_core,
 			 freeram,
 			 usedram,
 			 bufferram,
@@ -100,17 +176,19 @@ sysinfo_test(char      *instr,
 	goto done;
     if ((str = malloc(slen+1)) == NULL)
 	goto done;
-    snprintf(str, slen+1, 
+    snprintf(str, slen+1,
 	     "<uptime>%lu</uptime>"
 	     "<loads>%lu.%lu</loads>"
+	     "<loadcore>%f</loadcore>"
 	     "<freeram>%lu</freeram>"
 	     "<usedram>%lu</usedram>"
 	     "<bufferram>%lu</bufferram>"
 	     "<procs>%lu</procs>"
 	     "<freeswap>%lu</freeswap>"
-	     "<usedswap>%lu</usedswap>", 
+	     "<usedswap>%lu</usedswap>",
 	     uptime,
 	     loads/DEC_3, loads%DEC_3,
+	     load_core,
 	     freeram,
 	     usedram,
 	     bufferram,
@@ -135,15 +213,17 @@ grideye_plugin_init(int version)
 
 
 #ifndef _NOMAIN
-int main() 
+int main(int argc, char **argv)
 {
     char   *str = NULL;
 
     if (grideye_plugin_init(2) == NULL)
 	return -1;
-    if (sysinfo_test(0, &str) < 0)
-	return -1;
-    if (str){
+
+    if (sysinfo_test(NULL, &str) < 0)
+	    return -1;
+
+    if (str) {
 	fprintf(stdout, "%s\n", str);
 	free(str);
     }
