10274833fdaecac24218776cdb8b3c78fdd8bb07
Kristofer Hallin <kristofer.hallin@cloudmon360.com>
2018-04-28 20:44:39 +0200

    Updated buildscript

--- util/build_debian.sh
+++ util/build_debian.sh
@@ -2,7 +2,7 @@
 
 # Install dependencies
 apt-get update
-apt-get install -y libcurl4-nss-dev gcc make bison flex git
+apt-get install -y libfcgi-dev curl sysstat libcurl4-nss-dev gcc make bison flex git
 
 # Clone repos
 mkdir grideye_build
@@ -10,24 +10,25 @@ mkdir grideye_build
 BASEDIR="`pwd`/grideye_build"
 
 cd $BASEDIR
-
 git clone https://github.com/clicon/clixon.git
 git clone https://github.com/olofhagsand/cligen.git
 git clone https://github.com/cloudmon360/grideye_agent.git
+
+cd $BASEDIR/clixon
+git checkout -b develop origin/develop
+./configure
+make
+make install
+make install-include
 
 cd $BASEDIR/cligen
 ./configure
 make
 make install
 
-cd $BASEDIR/clixon
-./configure --without-restconf --without-keyvalue
-make
-make install
-make install-include
-
 cd $BASEDIR/grideye_agent
 ./configure
+sed -i -- 's/0 \/\* Verify/1 \/\* Verify/' grideye_agent.c
 make
 make install
 
